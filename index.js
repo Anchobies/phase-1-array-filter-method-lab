e6e6414109b1a14f22519e35d727ebf2e62c34a7
Anchobies <anthonychung97@gmail.com>
2021-05-18 22:26:24 -0400

    commit tree 2ae0344195aeb07444aa1398443e12043a1ee7f6

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+const findMatching = (drivers, keywords) => {
+    return drivers.filter(function (element) { return element.toUpperCase() === keywords.toUpperCase()});
+}
+
+const fuzzyMatch = (drivers, keywords) => drivers.filter(function (element) { return element.indexOf(keywords) === 0 });
+
+const matchName = (drivers, keywords) => drivers.filter(function (element) { return element.name === keywords });
